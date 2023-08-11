d5e34cf95bce7bdbb92b72d6e623668cc31a08cd
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-11 17:52:51 +0530

    login done

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const EncodeData = require("../utils/func");
+const DecodeData = require("../utils/func");
 
 // register
 router.post("/register", async (req, res) => {
@@ -14,7 +15,21 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json({ message: "User not found" });
+
+    const validPasswd = await DecodeData(req.body.password, user.password);
+    !validPasswd && res.status(400).json({ message: "Invalid password" });
+
+    res.status(200).json({ message: "Successfull" });
+  } catch (error) {
     res.status(500).json(error);
   }
 });
--- utils/func.js
+++ utils/func.js
@@ -1,9 +1,17 @@
 const bcrypt = require("bcrypt");
 
+// encode data
 async function EncodeData(data) {
   const salt = await bcrypt.genSalt(10);
   const encodedData = await bcrypt.hash(data, salt);
   return encodedData;
 }
 
+// check encoded data & data
+async function DecodeData(data, encodedData) {
+  const result = await bcrypt.compare(data, encodedData);
+  return result;
+}
+
 module.exports = EncodeData;
+module.exports = DecodeData;
