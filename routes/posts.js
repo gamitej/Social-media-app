67e21381e52a1f183487eeb7bf08e5dbe2e76be6
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-12 12:06:52 +0530

    like and unlike post done

--- routes/posts.js
+++ routes/posts.js
@@ -41,8 +41,21 @@ router.delete("/:id", async (req, res) => {
 });
 
 // like a post
-router.post("/", (req, res) => {
-  res.send("done");
+router.put("/:id/like-unlike-post", async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "No such post" });
+
+    if (post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      return res.status(200).json({ message: "Post unliked" });
+    }
+    await post.updateOne({ $push: { likes: req.body.userId } });
+    return res.status(200).json({ message: "Post liked" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get a post
