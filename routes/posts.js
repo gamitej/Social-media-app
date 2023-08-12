c9e70807194c9ddbd37f9448dcb9df9797873ae8
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-12 12:55:29 +0530

    get post

--- routes/posts.js
+++ routes/posts.js
@@ -60,8 +60,12 @@ router.put("/:id/like", async (req, res) => {
 
 // get a post
 router.get("/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  return res.status(200).json(post);
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
